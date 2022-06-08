fe8d78b68f819feaefa9ce4f0b19f43e7cccf46f
supto13 <parvez@unm.edu>
2022-06-07 22:32:13 -0700

    Added metadata info and explanation for parameters.

--- c++/config.h
+++ c++/config.h
@@ -1,18 +1,18 @@
 
-#define ROW 64
-#define COL 2000
+#define ROW 64 // number of vertical channels in the sensor
+#define COL 2000 // number of horizontal channels in the sensor
 
-int totalMinute = 10;
+int totalMinute = 10; // total data duration
 int nPoints = ROW * COL;
-int nFrames = 600;
-double coeffThreshold = 1.0;
+int nFrames = 600; // number of generated frames in each minute
+double coeffThreshold = 1.0; // t_w
 int neighSz = 30;
-double simThreshold = 0.999;
+double simThreshold = 0.999; // t_c
 
 std::string file_pref = "0000000000";
 
-std::string loc = "/home/supto/Research/LIDAR/simulation_data_frames/";
-std::string decompLoc = "decompressed/";
+std::string loc = ""; //data frames location
+std::string decompLoc = ""; // decompressed frames location
 
 
 
